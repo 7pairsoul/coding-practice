9b52d1fc6d7e2db507a053df314c2abc82796da9
7pairsoul <197509070+7pairsoul@users.noreply.github.com>
2025-03-21 15:05:49 +0500

    Simple isOdd with user input

--- isEven.js
+++ isEven.js
@@ -1,1 +1,15 @@
-// init repo
+const readline = require("readline"); // For getting user input
+
+function isOdd(n) {
+  return Math.abs(n % 2) == 1;
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Give me a number: ", function (answer) {
+  console.log(`The number you gave me is ${isOdd(answer) ? 'odd' : 'even'}`);
+  rl.close();
+});
